46ac3d463bb7f43ef0719b36a19da765a9a7deff
Andriy Pashko <smitas4511@gmail.com>
2020-04-10 15:44:48 +0300

    Updates

---
+++ precache-manifest.43926bd13b532256267bc84bf65b895d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7da4b1efa2908f8a7c4a6735e119480",
+    "url": "/nerdysoft/index.html"
+  },
+  {
+    "revision": "5a4f0010947e87585a2d",
+    "url": "/nerdysoft/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc2129d2eeacb6f9289c",
+    "url": "/nerdysoft/static/js/2.f751f623.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nerdysoft/static/js/2.f751f623.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a4f0010947e87585a2d",
+    "url": "/nerdysoft/static/js/main.8f4ba784.chunk.js"
+  },
+  {
+    "revision": "0c2919afb0fd553b2f4d",
+    "url": "/nerdysoft/static/js/runtime-main.d5de7d4f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nerdysoft/static/media/logo.5d5d9eef.svg"
+  }
+]);
